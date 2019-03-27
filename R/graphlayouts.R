9db963b702562de2629848b6ad2b07a18322a6ec
schochastics <david@schochastics.net>
2019-03-27 09:43:04 +0000

    added

---
+++ R/graphlayouts.R
@@ -0,0 +1,19 @@
+#' graphlayouts: layout algorithms for network visualizations
+#'
+#' @description The package implements several new layout algorithms to visualize networks.
+#' Most are based on the concept of stress majorization. Some more specific algorithms allow to emphasize
+#' hidden group structures in networks or focus on specific nodes. The package is best used in conjunction with
+#' ggraph.
+#'
+#' @details Some features of the package are:
+#'
+#' \itemize{
+#' \item `layout_with_stress()` is a state of the art deterministic layout algorithms.
+#' \item `layout_as_backbone()` uncovers hidden group structures (if they exist) by emphasizing strongly embedded edges.
+#' \item `qplot()` is a convenience function to quickly produce a visualization of a network.
+#' \item `layout_with_focus()` and `layout_with_centrality()` produce concentric layouts with a focal or most central nodes in the center.
+#' }
+#'
+#' @docType package
+#' @name graphlayouts
+NULL
