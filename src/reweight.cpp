ed7a5f8f75edcbf1ab01dd98cb0e578fafe677dd
schochastics <david@schochastics.net>
2024-03-11 21:32:59 +0100

    more rewrite

--- src/reweight.cpp
+++ src/reweight.cpp
@@ -1,5 +1,7 @@
 #include <Rcpp.h>
 using namespace Rcpp;
+#include <algorithm>  // For std::set_intersection
+#include <iterator>   // For std::back_inserter
 
 List sort_first_col(List matrices) {
   int n = matrices.size();
@@ -8,11 +10,33 @@ List sort_first_col(List matrices) {
   for (int i = 0; i < n; ++i) {
     IntegerMatrix mat = as<IntegerMatrix>(matrices[i]);
     IntegerVector firstColumn = mat(_, 0);
-    std::sort(firstColumn.begin(), firstColumn.end());
-    sorted_list[i] = firstColumn;
+    sorted_list[i] = firstColumn.sort();
   }
 
   return sorted_list;
+}
+
+IntegerVector union_intersect(const IntegerVector& v1,
+                              const IntegerVector& v2) {
+  int i = 0, j = 0, intersectionCount = 0, unionCount = 0;
+  while (i < v1.size() && j < v2.size()) {
+    if (v1[i] < v2[j]) {
+      ++unionCount;
+      ++i;
+    } else if (v1[i] > v2[j]) {
+      ++unionCount;
+      ++j;
+    } else {
+      ++intersectionCount;
+      ++unionCount;
+      ++i;
+      ++j;
+    }
+  }
+  // Add remaining elements from either vector to the union count
+  unionCount += (v1.size() - i) + (v2.size() - j);
+  IntegerVector counts = IntegerVector::create(intersectionCount, unionCount);
+  return counts;
 }
 
 // [[Rcpp::export]]
@@ -30,6 +54,7 @@ NumericVector reweighting(IntegerMatrix el, List N_ranks) {
   int maxv;
   int maxi;
   NumericVector new_w(m);
+  List Nr_sorted = sort_first_col(N_ranks);
   for (int e = 0; e < m; ++e) {
     u = el(e, 0);
     v = el(e, 1);
@@ -38,8 +63,9 @@ NumericVector reweighting(IntegerMatrix el, List N_ranks) {
     maxu = Nru(Nru.nrow() - 1, 1);  // Rcpp::max(Nru(_, 1));
     maxv = Nrv(Nrv.nrow() - 1, 1);  // Rcpp::max(Nrv(_, 1));
     maxi = max(IntegerVector::create(maxu, maxv));
-    Nru_vec = Nru(_, 0);
-    Nrv_vec = Nrv(_, 0);
+
+    Nru_vec = as<IntegerVector>(Nr_sorted[u]);  // Nru(_, 0);
+    Nrv_vec = as<IntegerVector>(Nr_sorted[v]);  // Nrv(_, 0);
     double jac_max = 0;
     for (int i = 0; i < maxi; ++i) {
       if (i > maxu) {
@@ -52,12 +78,14 @@ NumericVector reweighting(IntegerMatrix el, List N_ranks) {
       } else {
         vi = i;
       }
-      IntegerVector Nru_sort = Nru_vec[seq_len(ui)];
-      IntegerVector Nrv_sort = Nrv_vec[seq_len(vi)];
-      Nru_sort = Nru_sort.sort();
-      Nrv_sort = Nrv_sort.sort();
-      int Nuv_int = intersect(Nru_sort, Nrv_sort).length();
-      int Nuv_tot = union_(Nru_sort, Nrv_sort).length();
+
+      IntegerVector Nru_vec_short = Nru_vec[seq_len(ui)];
+      IntegerVector Nrv_vec_short = Nrv_vec[seq_len(vi)];
+
+      IntegerVector tmp = union_intersect(Nru_vec_short, Nrv_vec_short);
+
+      int Nuv_int = tmp[0];
+      int Nuv_tot = tmp[1];
       double jac_temp = double(Nuv_int) / double(Nuv_tot);
       if (jac_temp > jac_max) {
         jac_max = jac_temp;
